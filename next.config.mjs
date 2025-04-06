9f78eefff1bc671cac481f941cd8f88bb658bda3
nicks <nikhs247@gmail.com>
2025-04-06 00:07:27 -0500

    assetPrefix

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
-    assetPrefix: process.env.NODE_ENV === 'production' ? `/` : '',
+    assetPrefix: process.env.NODE_ENV === 'production' ? `https://nsreekum.github.io/` : '',
     images: {
       unoptimized: true, // Required for static export
     },
