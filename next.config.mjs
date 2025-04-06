378e764d541c49aa7f0d377392707c7f5d5e8dab
nicks <nikhs247@gmail.com>
2025-04-06 01:28:09 -0500

    regen docs

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,12 @@
+const isProd = process.env.NODE_ENV === 'production'
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  basePath: '/docs',
+  assetPrefix: isProd ? '/docs' : '', // Set asset prefix to `/docs` in production
+  basePath: '',  // No base path needed for GitHub Pages user site
   trailingSlash: true,
   images: {
-    unoptimized: true,
+    unoptimized: true,  // Required for static export
   },
   output: 'export',
 }
