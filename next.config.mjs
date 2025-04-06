a4bb6ef36c3b5b12aeab2aecc9032e53117da488
nicks <nikhs247@gmail.com>
2025-04-06 10:28:37 -0500

    delete content

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,7 @@
-const isProd = process.env.NODE_ENV === 'production'
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  assetPrefix: isProd ? '/docs' : '', // Set asset prefix to `/docs` in production
-  basePath: '',  // No base path needed for GitHub Pages user site
+  assetPrefix: '',
+  basePath: '',
   trailingSlash: true,
   images: {
     unoptimized: true,  // Required for static export
