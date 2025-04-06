9b4e573053125dbb8497ca2fb86ad1d9347c6188
nicks <nikhs247@gmail.com>
2025-04-06 01:10:20 -0500

    basePath set to /docs

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,7 @@ const isProd = process.env.NODE_ENV === 'production'
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  basePath: '/docs',
   trailingSlash: true,
   images: {
     unoptimized: true,
